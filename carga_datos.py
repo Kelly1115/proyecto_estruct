d356346b3a58624865d81399218bf0a0968a19b4
Kelly1115 <kelly.merino@unach.edu.ec>
2025-06-26 16:54:40 -0500

    Carga de datos

--- carga_datos.py
+++ carga_datos.py
@@ -1,7 +1,7 @@
 import pandas as pd
 #De prueba
 # Lee el archivo Excel
-df = pd.read_excel("red_prestamos_interbancarios_unica_hoja (1).xlsx")  # Cambia el nombre por el de tu archivo
+df = pd.read_excel("red_prestamos_interbancarios.xlsx")  # Cambia el nombre por el de tu archivo
 
 df.head(51) # Muestra las primeras filas para analizar la estructura del datasetimport networkx as nx
 
